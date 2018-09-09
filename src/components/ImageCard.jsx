8efbcd9469ed2840cbbcccfa6665243574b52e70
Tejas <tejasrr19@gmail.com>
2018-09-08 17:50:54 -0700

    Intial ImageCard Component

---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ImageCard = ({image}) => {
+  const {_id, url, caption} = image;
+
+  return (
+    <div id={`imageCard-${_id}` className='image-card'}>
+      <div className='caption'>{caption}</div>
+      <img src={url} alt={'Image not available'}></img>
+
+    </div>
+  )
+}
+
+export default ImageCard;
